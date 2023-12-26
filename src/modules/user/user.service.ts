a68b86e6b765513121eb57a325f4602c5caa2609
Johurul Haque <johurulhaquejony@gmail.com>
2023-12-26 15:03:29 +0600

    snapshot

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,8 +1,8 @@
 import { env } from '@config';
 import { AppError } from '@utils';
-import { compare } from 'bcrypt';
+import { compare, hash } from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import { User } from './user.interface';
+import { PasswordChangePayload, TJwtPayload, User } from './user.interface';
 import { UserModel } from './user.model';
 
 export function register(payload: User) {
@@ -40,4 +40,38 @@ export async function login(payload: Pick<User, 'username' | 'password'>) {
     user,
     token,
   };
+}
+
+export async function changePassword(
+  jwtPayload: TJwtPayload,
+  payload: PasswordChangePayload
+) {
+  if (payload.currentPassword === payload.newPassword)
+    throw new AppError(
+      400,
+      'InvalidPasswordError',
+      'New password must be different from the current password.'
+    );
+
+  const user = await UserModel.findById(jwtPayload._id);
+
+  if (!user)
+    throw new AppError(
+      404,
+      'Not Found',
+      'User not found with the provided ID.'
+    );
+
+  const isMatched = await compare(payload.currentPassword, user.password);
+
+  if (!isMatched)
+    throw new AppError(
+      401,
+      'AuthenticationFailed',
+      "The provided password does not match the user's stored password. Please try again."
+    );
+
+  return UserModel.findByIdAndUpdate(jwtPayload._id, {
+    password: await hash(payload.newPassword, env.SALT_ROUNDS),
+  });
 }
