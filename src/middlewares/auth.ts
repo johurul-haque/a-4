63f32e2ecbe39a50de833ce092f3bb6af915191d
Johurul Haque <johurulhaquejony@gmail.com>
2023-12-27 15:38:40 +0600

    Added Authorization for admin

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -4,30 +4,38 @@ import { AppError, catchAsync } from '@utils';
 import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 
-export const auth = catchAsync(async function (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
-  const token = req.headers.authorization;
+export const auth = (role?: 'admin') => {
+  return catchAsync(async function (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    try {
+      const token = req.headers.authorization;
 
-  if (!token)
-    throw new AppError(
-      401,
-      'Unauthorized Access',
-      'You do not have the necessary permissions to access this resource.'
-    );
+      if (!token) throw new Error();
 
-  jwt.verify(token, env.JWT_SECRET, function (err, decoded) {
-    if (err)
+      jwt.verify(token, env.JWT_SECRET, function (err, decoded) {
+        if (err) throw new Error();
+
+        req.user = decoded as TJwtPayload;
+
+        if (role) {
+          if (req.user.role === role) {
+            next();
+          } else {
+            throw new Error();
+          }
+        } else {
+          next();
+        }
+      });
+    } catch (error) {
       throw new AppError(
         401,
         'Unauthorized Access',
         'You do not have the necessary permissions to access this resource.'
       );
-
-    req.user = decoded as TJwtPayload;
+    }
   });
-
-  next();
-});
+};
--- src/modules/category/category.route.ts
+++ src/modules/category/category.route.ts
@@ -1,3 +1,4 @@
+import { auth } from '@middlewares/auth';
 import { validateRequest } from '@middlewares/validate-request';
 import { Router } from 'express';
 import { createCategory, getCategories } from './category.controller';
@@ -6,6 +7,10 @@ import { categorySchema } from './category.interface';
 const router = Router();
 
 router.get('/', getCategories);
-router.post('/', validateRequest(categorySchema), createCategory);
+router.post(
+  '/',
+  [auth('admin'), validateRequest(categorySchema)],
+  createCategory
+);
 
 export const CategoryRoutes = router;
